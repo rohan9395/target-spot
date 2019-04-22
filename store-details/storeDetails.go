6211d552b9a9435b6469ac824652a073f4aca47f
Rubin Thomas <rubin.thomas@target.com>
2019-04-22 14:31:16 +0530

    read store timings from monday

--- store-details/storeDetails.go
+++ store-details/storeDetails.go
@@ -4,93 +4,95 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"github.com/Jeffail/gabs"
-	"github.com/rs/zerolog/log"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/Jeffail/gabs"
+	"github.com/rs/zerolog/log"
 )
+
 type HTTPClient struct {
 	client *http.Client
 	pool   *x509.CertPool
 }
 
-const storeurl  = "https://redsky.target.com/v2/stores/location/%s"
-const cityurl  = "https://redsky.target.com/v2/stores/nearby/%s?locale=en-US&limit=20&range=250"
+const storeurl = "https://redsky.target.com/v2/stores/location/%s"
+const cityurl = "https://redsky.target.com/v2/stores/nearby/%s?locale=en-US&limit=20&range=250"
 
-func GetStoreName(storeId string ) string {
+func GetStoreName(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	name := jsonParsed1.Index(0).Path("name").Data().(string)
 	return name
 }
 
-func GetStoreID(city string ) string {
+func GetStoreID(city string) string {
 
 	jsonParsed1 := makeStoreDetailsbyCity(city)
 	id := jsonParsed1.Path("ID").Data().(float64)
-	storeId := fmt.Sprintf("%d",int(id))
+	storeId := fmt.Sprintf("%d", int(id))
 	return storeId
 }
 
-func GetPharmacy(storeId string ) string {
+func GetPharmacy(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	capability := jsonParsed1.Path("capabilities").String()
 
-	id := strings.Contains(capability,"CVS pharmacy")
-	if id{
+	id := strings.Contains(capability, "CVS pharmacy")
+	if id {
 		return "Yes, CVS pharmacy is available in this store"
-	}else{
+	} else {
 		return "No, pharmacy is not available in this store"
 	}
 
 }
 
-func GetStarbucks(storeId string ) string {
+func GetStarbucks(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	capability := jsonParsed1.Path("capabilities").String()
 
-	id := strings.Contains(capability,"Starbucks")
-	if id{
+	id := strings.Contains(capability, "Starbucks")
+	if id {
 		return "Yes, Starbucks is available in this store"
-	}else{
+	} else {
 		return "No, Starbucks is not available in this store"
 	}
 
 }
 
-func GetFresh(storeId string ) string {
+func GetFresh(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	capability := jsonParsed1.Path("capabilities").String()
 
-	id := strings.Contains(capability,"Fresh Grocery")
-	if id{
+	id := strings.Contains(capability, "Fresh Grocery")
+	if id {
 		return "Yes, Fresh Grocery is available in this store"
-	}else{
+	} else {
 		return "No, Fresh Grocery is not available in this store"
 	}
 
 }
 
-func GetPhotoLab(storeId string ) string {
+func GetPhotoLab(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	capability := jsonParsed1.Path("capabilities").String()
 
-	id := strings.Contains(capability,"Photo Lab")
-	if id{
+	id := strings.Contains(capability, "Photo Lab")
+	if id {
 		return "Yes, Photo Lab is available in this store"
-	}else{
+	} else {
 		return "No, Photo Lab is not available in this store"
 	}
 
 }
 
-func GetStorePhone(storeId string ) string {
+func GetStorePhone(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	TelephoneNumber := jsonParsed1.Index(0).Path("phoneNumbers")
@@ -99,8 +101,7 @@ func GetStorePhone(storeId string ) string {
 	return "Phone Number of store is " + phone
 }
 
-
-func GetStoreAddress(storeId string ) string {
+func GetStoreAddress(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
 	address := jsonParsed1.Index(0).Path("address.formattedAddress").Data().(string)
@@ -108,24 +109,24 @@ func GetStoreAddress(storeId string ) string {
 	return "Stores address is " + address
 }
 
-func GetStoreTiming(storeId string ) string {
+func GetStoreTiming(storeId string) string {
 
 	jsonParsed1 := makeStoreDetails(storeId)
-	starttime := jsonParsed1.Index(0).Path("operatingHours").Index(0).Path("timePeriod.beginTime").Data().(string)
-	endtime := jsonParsed1.Index(0).Path("operatingHours").Index(0).Path("timePeriod.thruTime").Data().(string)
-	start := strings.Split(starttime,":")
-	end := strings.Split(endtime,":")
+	starttime := jsonParsed1.Index(0).Path("operatingHours").Index(1).Path("timePeriod.beginTime").Data().(string)
+	endtime := jsonParsed1.Index(0).Path("operatingHours").Index(1).Path("timePeriod.thruTime").Data().(string)
+	start := strings.Split(starttime, ":")
+	end := strings.Split(endtime, ":")
 	startint, _ := strconv.Atoi(start[0])
 	endint, _ := strconv.Atoi(end[0])
 
-	if len(start) >0 && len(end)>0{
+	if len(start) > 0 && len(end) > 0 {
 		return fmt.Sprintf("Stores opens at %d hours in the morning and closes at %d hours in the evening", startint, endint)
-	}else{
+	} else {
 		return "I can't find store timings"
 	}
 }
 
-func makeStoreDetailsbyCity(city string) *gabs.Container{
+func makeStoreDetailsbyCity(city string) *gabs.Container {
 	h := HTTPClient{}
 	url := fmt.Sprintf(cityurl, city)
 
@@ -153,7 +154,7 @@ func makeStoreDetailsbyCity(city string) *gabs.Container{
 
 }
 
-func makeStoreDetails(storeId string) *gabs.Container{
+func makeStoreDetails(storeId string) *gabs.Container {
 	h := HTTPClient{}
 	url := fmt.Sprintf(storeurl, storeId)
 
